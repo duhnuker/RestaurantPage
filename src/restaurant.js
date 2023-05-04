13987b8a1d51ed4f92c0552ca68efa2209417a43
duhnuker <dinukanh@gmail.com>
2023-05-04 14:11:20 +1000

    homepage layout set

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,12 +1,31 @@
 const createRestaurantHomePage = () => {
     const content = document.querySelector("#content");
+
     const pageContent = document.createElement('div');
     pageContent.classList.add('page-content');
+    content.appendChild(pageContent);
 
     const heading = document.createElement('h1');
-    heading.textContent = 'Ruby Pearl Roadhouse';
+    heading.textContent = 'Ruby Pearl';
     pageContent.appendChild(heading);
-    content.appendChild(pageContent);
+
+
+    const aboutContainer = document.createElement('div');
+    aboutContainer.classList.add('about-container');
+    content.appendChild(aboutContainer);
+    const aboutText = document.createElement('p');
+    aboutText.textContent = 'short quote';
+    aboutContainer.appendChild(aboutText);
+
+    const imgContainer = document.createElement('div');
+    const restaurantimg = document.createElement('img');
+    restaurantimg.src = "../src/assets/imgs/restaurantimg.jpg";
+    restaurantimg.classList.add('restImg');
+    imgContainer.appendChild(restaurantimg);
+    imgContainer.classList.add('img-container');
+    content.appendChild(imgContainer);
+
+
 }
 
 export default createRestaurantHomePage;
