35c06abb4e1823468b72715310f9122aaf2bdb50
duhnuker <dinukanh@gmail.com>
2023-05-04 16:18:11 +1000

    tabs created for home page, not complete

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,20 +1,19 @@
 const createRestaurantHomePage = () => {
     const content = document.querySelector("#content");
 
-    const pageContent = document.createElement('div');
-    pageContent.classList.add('page-content');
-    content.appendChild(pageContent);
-
+    const headContainer = document.createElement('div');
+    headContainer.classList.add('head-container');
+    content.appendChild(headContainer);
     const heading = document.createElement('h1');
-    heading.textContent = 'Ruby Pearl';
-    pageContent.appendChild(heading);
+    heading.textContent = 'Ruby Pearl Diner';
+    headContainer.appendChild(heading);
 
 
     const aboutContainer = document.createElement('div');
     aboutContainer.classList.add('about-container');
     content.appendChild(aboutContainer);
     const aboutText = document.createElement('p');
-    aboutText.textContent = 'short quote';
+    aboutText.textContent = 'Welcome ';
     aboutContainer.appendChild(aboutText);
 
     const imgContainer = document.createElement('div');
@@ -24,6 +23,10 @@ const createRestaurantHomePage = () => {
     imgContainer.appendChild(restaurantimg);
     imgContainer.classList.add('img-container');
     content.appendChild(imgContainer);
+
+    const footerContainer = document.createElement('div');
+    footerContainer.classList.add('footer-container');
+    content.appendChild(footerContainer);
 
 
 }
---
+++ src/tabs.js
@@ -0,0 +1,39 @@
+import createRestaurantHomePage from "./restaurant";
+import createMenuPage from "./menu";
+import createContactPage from "./contact";
+
+const createTabs = () => {
+    const content = document.querySelector('#content');
+
+    const div1 = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+
+    div1.setAttribute('id', 'home-btn');
+    div2.setAttribute('id', 'menu-btn');
+    div3.setAttribute('id', 'contact-btn');
+
+    div1.classList.add('tab');
+    div2.classList.add('tab');
+    div3.classList.add('tab');
+
+    div1.textContent = 'Home';
+    div2.textContent = 'Menu';
+    div3.textContent = 'Contact';
+
+    content.appendChild(div1);
+    content.appendChild(div2);
+    content.appendChild(div3);
+
+    div1.addEventListener('click', () => {
+        createRestaurantHomePage();
+    })
+    div2.addEventListener('click', () => {
+        createMenu();
+    })
+    div3.addEventListener('click', () => {
+        createContact();
+    })
+}
+
+export default createTabs;
