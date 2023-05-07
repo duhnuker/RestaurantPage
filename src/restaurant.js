a28421a9e95ca94e637e43ef4f3b7baa295ba81e
Dinuka <dinukanh@gmail.com>
2023-05-07 23:17:59 +1000

    homepage styling done, will probably change

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,7 +3,10 @@ const createRestaurantHomePage = () => {
     const pageContent = document.createElement('div')
     pageContent.classList.add("page-content");
     content.appendChild(pageContent);
-    
+
+    const tabContainer = document.createElement('div');
+    tabContainer.classList.add('tab-container');
+    pageContent.appendChild(tabContainer);
 
     const headContainer = document.createElement('div');
     headContainer.classList.add('head-container');
@@ -16,8 +19,13 @@ const createRestaurantHomePage = () => {
     const aboutContainer = document.createElement('div');
     aboutContainer.classList.add('about-container');
     pageContent.appendChild(aboutContainer);
+    const aboutWelcomeText = document.createElement('p');
+    aboutWelcomeText.classList.add('about-welcome');
+    aboutWelcomeText.textContent = 'Welcome';
+    aboutContainer.appendChild(aboutWelcomeText);
     const aboutText = document.createElement('p');
-    aboutText.textContent = 'Welcome ';
+    aboutText.classList.add('about-description');
+    aboutText.textContent = 'Discover our modern Afro-centric dining, with a range of curated menus - from our exlusive line of WAGYU by Ruby Pearl, to the delectable South Australian Wine list.';
     aboutContainer.appendChild(aboutText);
 
     const footerContainer = document.createElement('div');
