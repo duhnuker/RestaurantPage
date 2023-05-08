4efde1b96fed8bb91b5ac9710fea1df76de20b2a
Dinuka <dinukanh@gmail.com>
2023-05-08 23:35:27 +1000

    contact page done

--- src/contact.js
+++ src/contact.js
@@ -5,24 +5,49 @@ const createRestaurantHomePage = () => {
     content.appendChild(pageContent);
     
 
-    const headContainer = document.createElement('div');
-    headContainer.classList.add('head-container');
-    pageContent.appendChild(headContainer);
-    const heading = document.createElement('h1');
-    heading.textContent = 'Ruby Pearl Diner';
-    headContainer.appendChild(heading);
+    const contactContainer = document.createElement('div');
+    contactContainer.classList.add('contact-container');
+    pageContent.appendChild(contactContainer);
+    const contactHeading = document.createElement('h3');
+    contactHeading.textContent = 'Contact';
+    contactContainer.appendChild(contactHeading);
 
 
-    const aboutContainer = document.createElement('div');
-    aboutContainer.classList.add('about-container');
-    pageContent.appendChild(aboutContainer);
-    const aboutText = document.createElement('p');
-    aboutText.textContent = 'Welcome ';
-    aboutContainer.appendChild(aboutText);
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form-container');
+    pageContent.appendChild(formContainer);
 
-    const footerContainer = document.createElement('div');
-    footerContainer.classList.add('footer-container');
-    pageContent.appendChild(footerContainer);
+    const form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', 'submit.php');
+    formContainer.appendChild(form);
+
+    const FN = document.createElement("input");
+    FN.classList.add('full-name');
+    FN.setAttribute("type", "text");
+    FN.setAttribute("name", "FullName");
+    FN.setAttribute("placeholder", "Full Name");
+    form.appendChild(FN);
+
+    const EID = document.createElement("input");
+    EID.classList.add('email');
+    EID.setAttribute("type", "text");
+    EID.setAttribute("name", "emailID");
+    EID.setAttribute("placeholder", "E-Mail");
+    form.appendChild(EID);
+
+    const ask = document.createElement("input");
+    ask.classList.add('input-ask');
+    ask.setAttribute("type", "textarea");
+    ask.setAttribute("placeholder", "How may we help?");
+    form.appendChild(ask);
+
+    const submit = document.createElement("input");
+    submit.classList.add('submit');
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Submit");
+    form.appendChild(submit);
+   
 
 
 }
