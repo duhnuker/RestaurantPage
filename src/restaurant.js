400ae5fecad7ea2fe8ddfbc9a1d87976e9927840
Dinuka <dinukanh@gmail.com>
2023-05-08 14:14:07 +1000

    home page done

--- src/restaurant.js
+++ src/restaurant.js
@@ -4,10 +4,6 @@ const createRestaurantHomePage = () => {
     pageContent.classList.add("page-content");
     content.appendChild(pageContent);
 
-    const tabContainer = document.createElement('div');
-    tabContainer.classList.add('tab-container');
-    pageContent.appendChild(tabContainer);
-
     const headContainer = document.createElement('div');
     headContainer.classList.add('head-container');
     pageContent.appendChild(headContainer);
@@ -15,23 +11,17 @@ const createRestaurantHomePage = () => {
     heading.textContent = 'Ruby Pearl';
     headContainer.appendChild(heading);
 
-
     const aboutContainer = document.createElement('div');
     aboutContainer.classList.add('about-container');
     pageContent.appendChild(aboutContainer);
     const aboutWelcomeText = document.createElement('p');
     aboutWelcomeText.classList.add('about-welcome');
-    aboutWelcomeText.textContent = 'Welcome';
+    aboutWelcomeText.textContent = 'Welcome,';
     aboutContainer.appendChild(aboutWelcomeText);
     const aboutText = document.createElement('p');
     aboutText.classList.add('about-description');
-    aboutText.textContent = 'Discover our modern Afro-centric dining, with a range of curated menus - from our exlusive line of WAGYU by Ruby Pearl, to the delectable South Australian Wine list.';
+    aboutText.textContent = 'Discover our modern Afro-centric dining, with a range of curated menus - from our exlusive line of WAGYU by Ruby Pearl, to our delectable selection of South Australian Wine.';
     aboutContainer.appendChild(aboutText);
-
-    const footerContainer = document.createElement('div');
-    footerContainer.classList.add('footer-container');
-    pageContent.appendChild(footerContainer);
-
 
 }
 
